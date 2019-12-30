1089be0cdc95b50c792e6af7aa8cb5d00d15239c
Samnier <romaykin.erik@gmail.com>
2019-12-30 18:35:46 +0600

    espresso

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ import sys
 import sqlite3
 
 
+con = sqlite3.connect('coffee.db')
+cur = con.cursor()
+
+
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
-        self.con = sqlite3.connect('coffee.db')
-        self.cur = self.con.cursor()
-        res = self.cur.execute("""select * from coffee""")
+        self.pushButton.clicked.connect(self.edit)
+        self.pushButton_2.clicked.connect(self.refresh)
+        res = cur.execute("""select * from coffee""")
         from PyQt5.QtWidgets import QTableWidgetItem
         self.tableWidget.setColumnCount(6)
         for j, i in enumerate(res):
@@ -22,6 +26,51 @@ class Example(QMainWindow):
             self.tableWidget.setItem(j, 4, QTableWidgetItem(str(i[4])))
             self.tableWidget.setItem(j, 5, QTableWidgetItem(str(i[5])))
             self.tableWidget.setItem(j, 6, QTableWidgetItem(str(i[6])))
+
+    def edit(self):
+        ex = AddWindow(self)
+        ex.show()
+
+    def refresh(self):
+        self.tableWidget.clear()
+        res = cur.execute("""select * from coffee""")
+        from PyQt5.QtWidgets import QTableWidgetItem
+        self.tableWidget.setColumnCount(6)
+        for j, i in enumerate(res):
+            self.tableWidget.setRowCount(j + 1)
+            self.tableWidget.setItem(j, 0, QTableWidgetItem(str(i[0])))
+            self.tableWidget.setItem(j, 1, QTableWidgetItem(str(i[1])))
+            self.tableWidget.setItem(j, 2, QTableWidgetItem(str(i[2])))
+            self.tableWidget.setItem(j, 3, QTableWidgetItem(str(i[3])))
+            self.tableWidget.setItem(j, 4, QTableWidgetItem(str(i[4])))
+            self.tableWidget.setItem(j, 5, QTableWidgetItem(str(i[5])))
+            self.tableWidget.setItem(j, 6, QTableWidgetItem(str(i[6])))
+
+
+class AddWindow(QMainWindow):
+    def __init__(self, parent):
+        super().__init__(parent)
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.pushButton.clicked.connect(self.add)
+        self.pushButton_2.clicked.connect(self.dl)
+
+    def add(self):
+        sort = self.lineEdit.text()
+        roast = self.lineEdit_2.text()
+        type = self.lineEdit_3.text()
+        desc = self.lineEdit_4.text()
+        pr = self.lineEdit_5.text()
+        cap = self.lineEdit_6.text()
+        cur.execute(f"""insert into coffee(sort, roast, type, 
+                    description, price, capacity)
+                    values('{sort}', {roast}, '{type}', 
+                    '{desc}', {pr}, {cap})""")
+        con.commit()
+
+    def dl(self):
+        id = self.lineEdit_7.text()
+        cur.execute(f"""delete from coffee where id = {id}""")
+        con.commit()
 
 
 app = QApplication(sys.argv)
